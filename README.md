01a05d82a85f3e149c074054267c43005b311b4e
Michael Deeb <michaeljdeeb@gmail.com>
2015-12-17 12:45:01 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+![IndecisiveBot Logo](/resources/indecisive-bot.png) indecisive-bot
+===
+A [Telegram](https://telegram.org) bot to help you decide what to eat. Written using [node-telegram-api](https://github.com/mdibaiee/node-telegram-api).
+
+Notes
+------
+- Functionality of this bot is still under development.
+- Pull requests to correct stylizing or inefficiencies are greatly apperciated!
+
+Setup
+------
+1. [Create](https://core.telegram.org/bots#create-a-new-bot) a new bot by chatting with [@TheBotFather](https://telegram.me/botfather) and keep the token handy.
+2. Populate [restaurants.json](restaurants.json) with content relevant to you.
+3. Install [Node.js & npm](https://nodejs.org)
+4. Install [telegram-api](https://www.npmjs.com/package/telegram-api) (This will be in a package.json file at some point)
+5. `TELEGRAM_BOT_TOKEN='YOUR_BOT_TOKEN' node main.js`
+6. Type 'IDK' to your bot.
+
+Demo
+------
+You can try out [@IndecisiveBot](https://telegram.me/IndecisiveBot) for yourself, but my restaurants.json might not be useful to you.
+
+![Screenshot](/resources/screenshot.png)
---
+++ main.js
@@ -0,0 +1,92 @@
+var Bot = require('telegram-api');
+var Message = require('telegram-api/types/Message');
+var Question = require('telegram-api/types/Question');
+var json = require('./restaurants.json');
+
+function getKeyboardOptions(jsonKey) {
+    var uniqueChoices = new Set();
+    for(var i=0; i<json.length; i++) {
+        if(Array.isArray(json[i][jsonKey])) {
+            for(var j=0; j<json[i][jsonKey].length; j++) {
+                uniqueChoices.add(json[i][jsonKey][j]);
+            }
+        } else {
+            uniqueChoices.add(json[i][jsonKey]);
+        }
+    }
+
+    uniqueChoices = Array.from(uniqueChoices).sort();
+    var listKeyboardFormat = [];
+    for(var i=0; i<uniqueChoices.length; i++) {
+        listKeyboardFormat.push([uniqueChoices[i]]);
+    }
+    listKeyboardFormat.push(['No Preference']);
+    return listKeyboardFormat;
+}
+
+var bot = new Bot({
+    token: process.env.TELEGRAM_BOT_TOKEN
+});
+
+bot.start().catch(err => {
+    console.log(err, '\n', err.stack);
+});
+
+bot.on('update', update => {
+    console.log('Polled\n', update);
+});
+
+const randomQuestion = new Question({
+    text: 'Should I just pick something at will?',
+    answers: [['Yes'], ['No']]
+});
+
+const serviceQuestion = new Question({
+    text: 'What kind of restaurant service would you like?',
+    answers: getKeyboardOptions('service')
+});
+
+const cuisineQuestion = new Question({
+    text: 'What kind of food are you in the mood for?',
+    answers: getKeyboardOptions('cuisine')
+});
+
+const distanceQuestion = new Question({
+    text: 'Are you willing to leave the neighborhood?',
+    answers: [['Yes'], ['No']]
+});
+
+bot.get('IDK', message => {
+    const id = message.chat.id;
+
+    randomQuestion.to(id).reply(message.message_id);
+
+    function recursiveRestaurantQuestion() {
+        const randomRestaurant = new Question({
+            text: 'How about ' + json[Math.floor(Math.random()*json.length)].name + '?',
+            answers: [['Yes'], ['No']]
+        });
+
+        randomRestaurant.to(id).reply(message.message_id);
+        bot.send(randomRestaurant).then(answer => {
+            if(answer.text == 'Yes') {
+                const msg = new Message().to(id).text('Enjoy your meal!');
+                bot.send(msg);
+            } else {
+                recursiveRestaurantQuestion();
+            }
+        }, () => {
+            const msg = new Message().to(id).text('Invalid answer');
+            bot.send(msg);
+        });
+    }
+
+    bot.send(randomQuestion).then(answer => {
+        if(answer.text == 'Yes') {
+            recursiveRestaurantQuestion();
+        }
+    }, () => {
+        const msg = new Message().to(id).text('Invalid answer');
+        bot.send(msg);
+    });
+});
